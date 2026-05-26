22f06097272fe738c24ad295f8fc5d2a09dce157
Vicențiu-Marian Ciorbaru <vicentiu@axdevlabs.com>
2026-05-26 11:43:25 +0300

    Document some kliquidity-sdk types (#88)

--- src/models/Holdings.ts
+++ src/models/Holdings.ts
@@ -1,15 +1,20 @@
 import Decimal from 'decimal.js';
 import { TokenAmounts } from './TokenAmounts';
 
-export type Holdings = {
-  available: TokenAmounts;
+// Token holdings for a strategy, including monetary value of the holdings
+export type Holdings = TokenHoldings & {
+  // The USD value of available tokens, 0 if the value cannot be calculated
   availableUsd: Decimal;
-  invested: TokenAmounts;
+  // USD value of invested tokens, 0 if the value cannot be calculated
   investedUsd: Decimal;
+  // USD value of all tokens, 0 if the value cannot be calculated
   totalSum: Decimal;
 };
 
+// Token holdings for a strategy
 export type TokenHoldings = {
+  // Number of tokens not yet invested in the DEX pool
   available: TokenAmounts;
+  // Number of tokens currently invested in the DEX pool
   invested: TokenAmounts;
 };
--- src/models/StrategyBalances.ts
+++ src/models/StrategyBalances.ts
@@ -4,9 +4,14 @@ import Decimal from 'decimal.js';
 import StrategyWithAddress from './StrategyWithAddress';
 
 export type StrategyBalances = {
+  // Token amounts & their USD value for the strategy
   computedHoldings: Holdings;
+  // Token price data, as viewed from on Chain
   prices: PriceData;
+  // Total amount of tokens in the strategy, including both available and invested tokens.
+  // Same as computedHoldings.available.a.plus(computedHoldings.invested.a)
   tokenAAmounts: Decimal;
+  // Same as computedHoldings.available.b.plus(computedHoldings.invested.b)
   tokenBAmounts: Decimal;
 };
 export type StrategyBalanceWithAddress = {
